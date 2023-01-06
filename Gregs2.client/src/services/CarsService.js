89018eba8e03c2b75dbaf467530c93a6c5407244
Andrew Greenlaw <a.greenlaw8@gmail.com>
2023-01-05 23:47:39 -0700

    getbyid

--- Gregs2.client/src/services/CarsService.js
+++ Gregs2.client/src/services/CarsService.js
@@ -9,6 +9,12 @@ class CarsService {
     AppState.cars = res.data.map(c => new Car(c))
     console.log('cars?', AppState.cars)
   }
+  async getCarById(id) {
+    const res = await api.get(`api/cars/${id}`)
+    console.log('[returned car]', res.data)
+    AppState.activeCar = new Car(res.data)
+    console.log(AppState.activeCar)
+  }
   async createCar(data) {
     const res = await api.post('api/cars', data)
     console.log('[created car]', res.data)
@@ -22,8 +28,6 @@ class CarsService {
     const index = AppState.cars.findIndex(c => c.id = id)
     AppState.cars.splice(index, 1, updatedCar)
     // Appstate.emit('cars')         do i need to do this? and Why?
-
-
   }
 } []
 export const carsService = new CarsService()

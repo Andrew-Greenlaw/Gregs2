c616125ed0a2eb039f2f8156823ea501410083d1
Andrew Greenlaw <a.greenlaw8@gmail.com>
2023-01-12 17:22:08 -0700

    jobs service

--- Gregs2.client/src/services/JobsService.js
+++ Gregs2.client/src/services/JobsService.js
@@ -15,6 +15,25 @@ class JobsService {
     AppState.activeJob = new Job(res.data)
     console.log('proxy obj job', AppState.activeJob)
   }
+  async createJob(data) {
+    const res = await api.post(`api/jobs`, data)
+    console.log(res.data)
+    AppState.jobs.push(new Job(res.data))
+  }
+  async editJob(id, data) {
+    const res = await api.put(`api/jobs/${id}`, data)
+    console.log('[Updated Job]', res.data)
+    const updatedJob = new Job(res.data)
+    const index = AppState.jobs.findIndex(j => j.id = id)
+    AppState.jobs.splice(index, 1, updatedJob)
+    // Appstate.emit('jobs')
+  }
+  async deleteJob(id) {
+    const res = await api.delete(`api/jobs/${id}`)
+    console.log('deleted?', res.data)
+    AppState.jobs.filter(j => j.id != id)
+    console.log(AppState.jobs)
+  }
 
 }
 export const jobsService = new JobsService()

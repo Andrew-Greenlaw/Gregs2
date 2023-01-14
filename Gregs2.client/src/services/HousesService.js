b230a42c27a70aae55d97f0bb5df95e32fa9e29b
Andrew Greenlaw <a.greenlaw8@gmail.com>
2023-01-13 20:49:56 -0700

    finished up the houses for now

--- Gregs2.client/src/services/HousesService.js
+++ Gregs2.client/src/services/HousesService.js
@@ -8,5 +8,28 @@ class HousesService {
     console.log('[houses]', res.data)
     AppState.houses = res.data.map(h => new House(h))
   }
+  async getHouseById(id) {
+    const res = await api.get(`api/houses/${id}`)
+    console.log('house?', res.data)
+    AppState.activeHouse = new House(res.data)
+  }
+  async createHouse(data) {
+    const res = await api.post('api/houses', data)
+    console.log(res.data)
+    AppState.houses.push(new House(res.data))
+  }
+  async editHouse(id, data) {
+    const res = await api.put(`api/houses/${id}`, data)
+    console.log(res.data)
+    const updatedHouse = new House(res.data)
+    const index = AppState.houses.findIndex(h => h.id = id)
+    AppState.houses.splice(index, 1, updatedHouse)
+    //Appsstate.emit('houses')
+  }
+  async deleteHouse(id) {
+    const res = await api.delete(`api/houses/${id}`)
+    AppState.houses.filter(h => h.id != id)
+    console.log('new houses array', AppState.houses)
+  }
 }
 export const housesService = new HousesService()
